bfc6c01eaf4ee25c8e3401cd62bf515506893b9d
cleanandsuite <sevend9llc@gmail.com>
2026-04-23 00:01:28 -0500

    Clean build

--- fix-build.ps1
+++
@@ -1,4 +0,0 @@
-$pkg = Get-Content package.json -Raw
-$pkg = $pkg -replace 'vite build && esbuild api/boot\.ts --platform=node --bundle --format=esm --outdir=dist --banner:js="import \{ createRequire \} from .module.;const require = createRequire\(import\.meta\.url\);"', 'vite build'
-$pkg | Set-Content package.json
-Write-Host "Done"
